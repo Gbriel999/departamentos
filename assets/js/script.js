250ae4b282e56ff8b199003e6fbb17d9d455de13
gabriel. <83030359+Gbriel999@users.noreply.github.com>
2024-11-04 00:32:47 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,68 @@
+import propiedadesArriendo from "./arriendo.js";
+import propiedadesVenta from "./venta.js";
+
+const propiedadesEnVenta = propiedadesVenta;
+const propiedadesEnArriendo = propiedadesArriendo;
+
+document.addEventListener("DOMContentLoaded", function () {
+  const contenedorVenta = document.getElementById("propiedadesEnVenta");
+  const contenedorArriendo = document.getElementById("propiedadesEnArriendo");
+
+  if (contenedorVenta) {
+    pintar(propiedadesEnVenta, contenedorVenta);
+  }
+
+  if (contenedorArriendo) {
+    pintar(propiedadesEnArriendo, contenedorArriendo);
+  }
+});
+
+function createCard(propiedades) {
+  let smokeIcon = '<i class="fas fa-smoking"></i>';
+  let smokeText = "Permitido fumar";
+  let petsIcon = '<i class="fas fa-paw"></i>';
+  let petsText = "Mascotas permitidas";
+
+  if (!propiedades.smoke) {
+    smokeIcon = '<i class="fas fa-smoking-ban text-danger"></i>';
+    smokeText = '<span class="text-danger">No se permite fumar</span>';
+  }
+
+  if (!propiedades.pets) {
+    petsIcon = '<i class="fa-solid fa-ban text-danger"></i>';
+    petsText = '<span class="text-danger">No se permiten mascotas</span>';
+  }
+
+  return `<div class="col-md-4 mb-4 h-100 mb-3 my-3">
+        <div class="card">
+            <img
+                src="${propiedades.src}"
+                class="card-img-top"
+                alt="Imagen del departamento"
+            />
+            <div class="card-body">
+                <h5 class="card-title">${propiedades.nombre}</h5>
+                <p class="card-text">${propiedades.descripcion}</p>
+                <p>
+                    <i class="fas fa-bed"></i> ${propiedades.habitaciones} Habitaciones |
+                    <i class="fas fa-bath"></i> ${propiedades.baños} Baños
+                </p>
+                <p><i class="fas fa-dollar-sign"></i>${propiedades.costo}</p>
+                <p class="text-success">
+                    ${smokeIcon} ${smokeText}
+                </p>
+                <p class="text-success">
+                    ${petsIcon} ${petsText}
+                </p>
+            </div>
+        </div>
+    </div>`;
+}
+
+function pintar(arreglo, contenedor) {
+  let html = "";
+  arreglo.forEach((element) => {
+    html += createCard(element);
+  });
+  contenedor.innerHTML = html;
+}
---
+++ assets/js/venta.js
@@ -0,0 +1,36 @@
+const propiedadesVenta = [
+  {
+    nombre: "Singular De Entrelomas",
+    src: "https://http2.mlstatic.com/D_NQ_NP_905643-MLC75804248227_042024-O.webp",
+    descripcion:
+      "Está ubicado en el mejor sector de Bosques de Montemar, a pasos del gran Parque La Foresta de Reñaca, un área verde de 28 mil m2 aprox., propicio para realizar actividad física al aire libre, recreación de niños y paseos de mascotas.",
+    habitaciones: 3,
+    banos: 3,
+    costo: 388105502,
+    smoke: false,
+    pets: true,
+  },
+  {
+    nombre: "Entrelomas 5",
+    src: "https://http2.mlstatic.com/D_NQ_NP_747516-MLC79656025414_102024-O.webp",
+    descripcion:
+      "Su ubicación estratégica hacen de Edificio Entrelomas 5 sea una atractiva opción entre quienes buscan vivir o invertir en proyectos que aseguran una alta calidad de vida.",
+    habitaciones: 3,
+    banos: 2,
+    costo: 145140825,
+    smoke: true,
+    pets: false,
+  },
+  {
+    nombre: "Playa Cochoa",
+    src: "https://http2.mlstatic.com/D_NQ_NP_615828-MLC79691392595_102024-O.webp",
+    descripcion:
+      "Un proyecto con una línea aterrazada, finas terminaciones y con un diseño vanguardista que te invita a sentir y disfrutar la brisa del mar.",
+    habitaciones: 2,
+    banos: 2,
+    costo: 363687514,
+    smoke: false,
+    pets: false,
+  },
+];
+export default propiedadesVenta;
